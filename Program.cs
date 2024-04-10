aca52dbbd582135e3a91c12ad00e5184fb8e9716
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-04-10 10:31:06 -0500

    user is prompted to give team members until empty name is given

--- Program.cs
+++ Program.cs
@@ -4,4 +4,55 @@
 //Create a team member for which will prompt the user to create a team member and save the created team members info
 //Print the team members information
 
-Console.WriteLine("Hello, World!");
+List<TeamMember> teamMembers = new List<TeamMember>();
+
+void Main()
+{
+    Console.WriteLine("Plan your heist!");
+    for (int i = 0; i < 100; i++)
+    {
+        TeamMember teamMember = TeamMemberForm();
+        if (teamMember == null)
+        {
+            break;
+        }
+        teamMembers.Add(teamMember);
+    }
+
+    Console.WriteLine(teamMembers.Count);
+    foreach (TeamMember teamMember in teamMembers)
+    {
+        Console.WriteLine($"{teamMember.Name} {teamMember.SkillLevel} {teamMember.CourageFactor}");
+    }
+    //display message containing the number of members of the team
+    //loop through team members
+    //make function that displays team member's information
+}
+
+TeamMember TeamMemberForm()
+{
+    string name;
+    int skillLevel;
+    decimal courageFactor;
+    Console.WriteLine("Enter a team member's name.");
+    name = Console.ReadLine();
+    if (name == "")
+    {
+        return null;
+    }
+    Console.WriteLine("Enter a skill level");
+    skillLevel = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter a courage factor (between 0 and 2)");
+    courageFactor = decimal.Parse(Console.ReadLine());
+
+    TeamMember teamMember = new TeamMember()
+    {
+        Name = name,
+        SkillLevel = skillLevel,
+        CourageFactor = courageFactor
+    };
+
+    return teamMember;
+}
+
+Main();
--- TeamMember.cs
+++ TeamMember.cs
@@ -1,13 +1,19 @@
 // Create a class for the team member, have a Name, Skill level as an int, and a courage factor as a decimal of 0.0 - 2.0
-// For courage factor write a setter function to only allow 0.0 - 2.0 
+// For courage factor write a setter function to only allow 0.0 - 2.0
 
 class TeamMember
 {
     public string Name { get; set; }
     public int SkillLevel { get; set; }
-    public decimal CourageFactor { get {return CourageFactor;} set {
-        value = Math.Max(0, value);
-        value = Math.Min(2, value);
-        CourageFactor = value;
-    }}
+    public decimal CourageFactor
+    {
+        get { return filteredCourageFactor; }
+        set
+        {
+            value = Math.Max(0, value);
+            value = Math.Min(2, value);
+            filteredCourageFactor = value;
+        }
+    }
+    private decimal filteredCourageFactor;
 }
