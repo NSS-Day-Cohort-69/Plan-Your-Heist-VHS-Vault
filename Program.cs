402355ce18e16d6a9394e15082e38a254896eab1
Michael Deitz <michaeljrdeitz@gmail.com>
2024-04-10 10:05:18 -0500

    created the class for the team member

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Create a main function
+//Print message Plan your heist
+//Create a team member for which will prompt the user to create a team member and save the created team members info
+//Print the team members information
+
 Console.WriteLine("Hello, World!");
---
+++ TeamMember.cs
@@ -0,0 +1,13 @@
+// Create a class for the team member, have a Name, Skill level as an int, and a courage factor as a decimal of 0.0 - 2.0
+// For courage factor write a setter function to only allow 0.0 - 2.0 
+
+class TeamMember
+{
+    public string Name { get; set; }
+    public int SkillLevel { get; set; }
+    public decimal CourageFactor { get {return CourageFactor;} set {
+        value = Math.Max(0, value);
+        value = Math.Min(2, value);
+        CourageFactor = value;
+    }}
+}
